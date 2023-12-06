480514e2f2a09b782b41210e144cf58b4eca4ed5
smohammade1383 <s.mohammad.e.1383@gmail.com>
2023-12-06 18:20:59 +0330

    calculator.c changed for 2nd time

--- calculator.c
+++ calculator.c
@@ -1,6 +1,6 @@
-#include<stdio.h>
-
-int calculate(int a,int b){
-	// Some calculation
-	return a*b;
+#include <stdio.h>
+int calculate(int a, int b) {
+ // A different calculation
+ return a + b;
 }
+
